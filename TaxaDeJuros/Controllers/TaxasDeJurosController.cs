c35acefb8935a0e171d1347602f482165c8ba7ee
Poliana Guimarães <poli998881189@gmail.com>
2022-05-11 21:20:12 -0300

    first commit

---
+++ TaxaDeJuros/Controllers/TaxasDeJurosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaxaDeJuros;
+
+namespace TaxasDeJuros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxasDeJurosController : ControllerBase
+    {
+
+        private readonly ILogger<TaxasDeJurosController> _logger;
+
+        public TaxasDeJurosController(ILogger<TaxasDeJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TaxasDeJurosResponse> TaxasDeJuros()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 1).Select(index => new TaxasDeJurosResponse
+            {
+                TaxasDeJuros = "A taxa de juros atual é de: 1 % ao mês!"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ TaxaDeJuros/TaxasDeJurosResponse.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TaxaDeJuros
+{
+    public class TaxasDeJurosResponse
+    {
+       public string TaxasDeJuros { get; set; }
+    }
+}
